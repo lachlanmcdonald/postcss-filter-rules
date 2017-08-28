ccb08396eab13433baeb009862ca2c283c3235c5
Lachlan McDonald <lachlan@radelaide.net>
2017-08-28 11:25:59 +1000

    RF let for var

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 /* jshint esversion: 6 */
 'use strict';
 
-var postcss = require('postcss');
+let postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-filter-rules', (options) => {
     options = options || {};
 
-    var cssSeparator = /\s+|\s*[~+>]\s*/,
+    let cssSeparator = /\s+|\s*[~+>]\s*/,
         defaultSafeAtRules = ['charset', 'import', 'keyframes'],
         removeAtRules = ['font-face', 'charset', 'import', 'keyframes'];
 
@@ -21,7 +21,7 @@ module.exports = postcss.plugin('postcss-filter-rules', (options) => {
 
     return (root) => {
         root.walkRules((rule) => {
-            var selectors = rule.selectors.slice().filter((selector) => {
+            let selectors = rule.selectors.slice().filter((selector) => {
                 return options.filter(selector, selector.split(cssSeparator));
             });
 
@@ -34,7 +34,7 @@ module.exports = postcss.plugin('postcss-filter-rules', (options) => {
 
         root.walkAtRules((rule) => {
             if (options.keepAtRules.indexOf(rule.name) === -1) {
-                var isEmpty = Array.isArray(rule.nodes) && rule.nodes.length === 0,
+                let isEmpty = Array.isArray(rule.nodes) && rule.nodes.length === 0,
                     removeByDefault = removeAtRules.indexOf(rule.name) >= 0;
 
                 if (isEmpty || removeByDefault) {
