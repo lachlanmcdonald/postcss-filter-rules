9f2fd89a863320477d0e4b9db14d6090960f86be
Lachlan McDonald <lachlanmcdonald@users.noreply.github.com>
2023-08-03 10:59:09 +0100

    RF Migrate to the updated postcss API

--- index.js
+++ index.js
@@ -3,45 +3,43 @@ const splitSelectors = require('./splitSelectors');
 const DEFAULT_SAFE_AT_RULES = ['charset', 'import', 'keyframes'];
 const REMOVE_AT_RULES = ['font-face', 'charset', 'import', 'keyframes'];
 
-const plugin = (options) => {
-	options = options || {};
+const plugin = (options = {}) => {
+	options = {
+		filter: () => true,
+		keepAtRules: DEFAULT_SAFE_AT_RULES,
+		splitFunction: splitSelectors,
+		...options,
+	};
 
-	options.filter = options.filter || (() => {
-		return true;
-	});
+	return {
+		Once(root, { result }) {
+			root.walkRules(rule => {
+				let selectors = rule.selectors.slice().filter(selector => {
+					return options.filter(selector, options.splitFunction(selector));
+				});
 
-	options.splitFunction = typeof options.splitFunction === 'function' ? options.splitFunction : splitSelectors;
-
-	if (options.keepAtRules !== true && Array.isArray(options.keepAtRules) === false) {
-		options.keepAtRules = DEFAULT_SAFE_AT_RULES;
-	}
-
-	return root => {
-		root.walkRules(rule => {
-			let selectors = rule.selectors.slice().filter(selector => {
-				return options.filter(selector, options.splitFunction(selector));
-			});
-
-			if (selectors.length === 0) {
-				rule.parent.removeChild(rule);
-			} else {
-				rule.selectors = selectors;
-			}
-		});
-
-		if (options.keepAtRules !== true) {
-			root.walkAtRules(rule => {
-				if (!options.keepAtRules.includes(rule.name)) {
-					let isEmpty = Array.isArray(rule.nodes) && rule.nodes.length === 0;
-					let removeByDefault = REMOVE_AT_RULES.includes(rule.name);
-
-					if (isEmpty || removeByDefault) {
-						rule.parent.removeChild(rule);
-					}
+				if (selectors.length === 0) {
+					rule.parent.removeChild(rule);
+				} else {
+					rule.selectors = selectors;
 				}
 			});
-		}
-	};
+
+			if (options.keepAtRules !== true) {
+				root.walkAtRules(rule => {
+					if (!options.keepAtRules.includes(rule.name)) {
+						let isEmpty = Array.isArray(rule.nodes) && rule.nodes.length === 0;
+						let removeByDefault = REMOVE_AT_RULES.includes(rule.name);
+
+						if (isEmpty || removeByDefault) {
+							rule.parent.removeChild(rule);
+						}
+					}
+				});
+			}
+		},
+		postcssPlugin: 'postcss-filter-rules',
+	}
 };
 
 plugin.postcss = true;
